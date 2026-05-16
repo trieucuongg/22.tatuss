f4931a452c11de407576184cd8616ca889f19436
22TATUSS <deploy@22tatuss.com>
2026-05-16 10:33:58 +0700

    Fix admin route to /admin

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html')
+      }
+    }
+  }
+});
